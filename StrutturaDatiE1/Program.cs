91345a9b25e5dca75f70ee4b784a6a49b39c52af
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:54:52 +0100

    Add project files.

---
+++ StrutturaDatiE1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace StrutturaDatiE1
+{
+    class Program
+    {
+        private const string file = @"FileDiTesto.txt"; 
+        private const string file1 = @"FileDiTesto1";
+        static void Main(string[] args)
+        {
+            List<double> lista = new List<double>();
+            using (StreamReader file = new StreamReader("FileDiTesto.txt"))
+            {
+                string Line;
+                while ((Line = file.ReadLine()) != null)
+                {
+                    lista.Add(double.Parse(Line));
+                }
+            }
+            using (StreamWriter file1 = new StreamWriter("FileDiTesto.txt"))
+            {
+                for (int i = lista.Count - 1; i >= 0; i--)
+                {
+                    file1.WriteLine(lista[i]);
+                }
+            }
+        }
+    }
+}
